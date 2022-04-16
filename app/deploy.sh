fe70771f4bea52061cdb1f3e297ea8ff34575817
SethAngell <accounts@sethangell.com>
2022-04-16 16:16:11 -0400

    tweak

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,4 +1,4 @@
 python manage.py collectstatic --no-input
 python manage.py makemigrations
-python managep.py migrate
+python manage.py migrate
 gunicorn homepage.wsgi:application --bind 0.0.0.0:8000
