a9a14954d3ec8131fd9a6d61efd88d08960bf658
SethAngell <accounts@sethangell.com>
2022-04-16 00:14:35 -0400

    -

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,2 +1,2 @@
-docker-compose build --no-cache --build-arg DB_NAME=$DB_NAME --build-arg DB_USER=$DB_USER --build-arg DB_PASS=$DB_PASS --build-arg DB_HOST=$DB_HOST --build-arg DB_PORT=$DB_PORT --build-arg S3_KEY=$S3_KEY --build-arg S3_SECRET_KEY=$S3_SECRET_KEY
+docker-compose build --build-arg DB_NAME=${DB_NAME} --build-arg DB_USER=${DB_USER} --build-arg DB_PASS=${DB_PASS} --build-arg DB_HOST=${DB_HOST} --build-arg DB_PORT=${DB_PORT} --build-arg S3_KEY=${S3_KEY} --build-arg S3_SECRET_KEY=${S3_SECRET_KEY} --no-cache 
 docker-compose up -d
