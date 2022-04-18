8ab422944a0c0deb3c25c541da2150edafb18d7c
sethangell <accounts@sethangell.com>
2022-04-18 17:19:51 +0000

    Tweaks to make things work serverside

--- app/homepage/settings.py
+++ app/homepage/settings.py
@@ -163,3 +163,25 @@ if DEBUG is False:
     X_FRAME_OPTIONS = "DENY"
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
