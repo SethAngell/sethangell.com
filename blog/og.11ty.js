a6888d35bde77e10571b542532f28f22247f2308
Seth Angell <accounts@sethangell.com>
2026-06-21 10:47:12 -0500

    fix(blog): exclude draft posts from OG card generation

--- blog/og.11ty.js
+++ blog/og.11ty.js
@@ -3,7 +3,12 @@ import { renderOgCard } from "../_lib/og-card.js";
 export default class {
 	data() {
 		return {
-			pagination: { data: "collections.blog", size: 1, alias: "post" },
+			pagination: {
+				data: "collections.blog",
+				size: 1,
+				alias: "post",
+				before: (data) => data.filter((post) => !post.data.draft),
+			},
 			permalink: (data) => `/blog/og/${data.post.fileSlug}.png`,
 			eleventyExcludeFromCollections: true,
 		};
